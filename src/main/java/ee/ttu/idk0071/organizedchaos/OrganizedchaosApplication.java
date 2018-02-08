4c76fcf6ce12441cf5e4eb7aa18d9c7a7a519027
kasparkivistik <kkivis@ttu.ee>
2018-02-08 10:26:09 +0200

    Initial commit

---
+++ src/main/java/ee/ttu/idk0071/organizedchaos/OrganizedchaosApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.idk0071.organizedchaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizedchaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizedchaosApplication.class, args);
+	}
+}
