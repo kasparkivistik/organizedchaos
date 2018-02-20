737923e10334650fff8fadfe09d5cfc4f419f0bf
keitarleen <keitarleenkalle@gmail.com>
2018-02-20 11:52:10 +0200

    Started with Add note functionality

--- organizedchaos-web/src/home/home.js
+++ organizedchaos-web/src/home/home.js
@@ -1,5 +1,20 @@
+import {Note} from './note';
 export class home {
   constructor(){
+    this.notes = [];
+    this.noteDescription = '';
+  }
+  addNote() {
+    if (this.noteDescription) {
+      this.notes.push(new Note(this.noteDescription));
+      this.noteDescription = '';
+    }
+  }
 
+  removeNote(note) {
+    let index = this.notes.indexOf(note);
+    if (index !== -1) {
+      this.notes.splice(index, 1);
+    }
   }
 }
