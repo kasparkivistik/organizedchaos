8eb6c908685eb1b4ccf31adb208780e520907e3b
keitarleen <keitarleenkalle@gmail.com>
2018-03-06 20:21:12 +0200

    Calendar files

--- organizedchaos-web/src/app.js
+++ organizedchaos-web/src/app.js
@@ -11,6 +11,7 @@ export class App {
     config.title = 'Chaos organized';
     config.map([
       {route: ['', 'home'], name: 'home', moduleId: 'home/home'},
+      {route: 'cal/calendar', name: 'calendar', moduleId: 'cal/calendar', title: 'Calendar', nav: true},
       {route: 'auth/login', name: 'login', moduleId: 'auth/login', title: 'Log in', nav: true},
       {route: 'auth/signup', name: 'signup', moduleId: 'auth/signup', title: 'Sign up', nav: true},
     ]);
