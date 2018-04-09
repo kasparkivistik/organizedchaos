af74f691c49c4d7bb165fec10308bca57fb37e4b
Eliise <eljuha@ttu.ee>
2018-04-09 17:50:53 +0300

    calendar is displayed

--- organizedchaos-web/src/cal/calendar.js
+++ organizedchaos-web/src/cal/calendar.js
@@ -1,22 +1,22 @@
+import $ from 'jquery';
+import 'fullcalendar';
+
 export class calendar {
 
   constructor(){
 
   }
+
+  attached() {
+    $('#calendar').fullCalendar({
+      header:{
+        right: "next",
+        center: "title",
+        left: "prev"
+      },
+      firstDay:1,
+      weekNumbers:1
+    })
+  }
 }
 
-$(function() {
-
-  // page is now ready, initialize the calendar...
-
-  $('#calendar').fullCalendar({
-    header:{
-      right: "next",
-      center: "title",
-      left: "prev"
-    },
-    firstDay:1,
-    weekNumbers:1
-  })
-
-});
