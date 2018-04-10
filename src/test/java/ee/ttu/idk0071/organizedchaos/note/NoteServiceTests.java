46bf06ac4feb913312a6a1781a3cc24cb0e09f73
keitarleen <keitarleenkalle@gmail.com>
2018-04-10 13:47:16 +0300

    NoteControllerTests - 2 passed tests

--- src/test/java/ee/ttu/idk0071/organizedchaos/note/NoteServiceTests.java
+++ src/test/java/ee/ttu/idk0071/organizedchaos/note/NoteServiceTests.java
@@ -1,11 +1,8 @@
 package ee.ttu.idk0071.organizedchaos.note;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import ee.ttu.idk0071.organizedchaos.user.User;
 import org.apache.catalina.filters.CorsFilter;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,28 +16,25 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(NoteController.class)
-public class NoteControllerTest {
+public class NoteServiceTests {
 
-    private static final long TEST_NOTE_ID1 = 1;
-    private static final long TEST_NOTE_ID2 = 2;
-    private static final long TEST_USER_ID = 1;
+    private static final long TEST_NOTE_ID1 = 1L;
+    private static final long TEST_NOTE_ID2 = 2L;
+    private static final long TEST_USER_ID = 1L;
 
     @Autowired
     private MockMvc mockMvc;
@@ -54,43 +48,12 @@ public class NoteControllerTest {
     private Note sampleNote1;
     private Note sampleNote2;
 
-    private User sampleUser1Id;
+    private User sampleUser1;
+
+    private List<Note> sampleNotes;
 
     @Before
     public void setup() {
-        /*sampleNote1 = new Note();
-        sampleNote1.setId(TEST_NOTE_ID1);
-        sampleNote1.setContent("Content");
-        sampleNote1.setName("Title");
-        sampleNote1.setComplete(true);
-
-        sampleNote2 = new Note();
-        sampleNote2.setId(TEST_NOTE_ID2);
-        sampleNote2.setContent("Content2");
-        sampleNote2.setName("Title2");
-        sampleNote2.setComplete(false);
-
-        sampleUser1 = new User();
-        sampleNote1.setUser(sampleUser1);
-
-        List<Note> sampleNotes = new ArrayList<>();
-        sampleNotes.add(sampleNote1);
-        sampleNotes.add(sampleNote2);
-
-
-        given(noteService.getAllNotes()).willReturn(sampleNotes);
-        given(noteService.getNotesByUser(sampleUser1)).willReturn(sampleNotes);
-        //given(this.notes.findOne(TEST_NOTE_ID)).willReturn(sampleNote);*/
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(noteController)
-                .addFilters(new CorsFilter())
-                .build();
-        
-    }
-
-    @Test
-    public void testGetAllNotesSuccess() throws Exception {
         sampleNote1 = new Note();
         sampleNote1.setId(TEST_NOTE_ID1);
         sampleNote1.setContent("Content");
@@ -103,28 +66,42 @@ public class NoteControllerTest {
         sampleNote2.setName("Title2");
         sampleNote2.setComplete(false);
 
-        sampleUser1Id = new User();
-        sampleUser1Id.setId(TEST_USER_ID);
-        sampleNote1.setUser(sampleUser1Id);
+        sampleUser1 = new User();
+        sampleUser1.setId(TEST_USER_ID);
+        sampleNote1.setUser(sampleUser1);
 
-        List<Note> sampleNotes = new ArrayList<>();
+        sampleNotes = new ArrayList<>();
         sampleNotes.add(sampleNote1);
         sampleNotes.add(sampleNote2);
+
+        given(noteService.getAllNotes()).willReturn(sampleNotes);
+        given(noteService.getNotesByUser(sampleUser1)).willReturn(sampleNotes);
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(noteController)
+                .addFilters(new CorsFilter())
+                .build();
+
+    }
+
+    @Test
+    public void testGetAllNotesSuccess() throws Exception {
 
         Mockito.when(noteService.getAllNotes()).thenReturn(sampleNotes);
         mockMvc.perform(get("/notes"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(1L)))
-                .andExpect(jsonPath("$[0].content", is("Content")))
-                .andExpect(jsonPath("$[0].name", is("Title")))
-                .andExpect(jsonPath("$[0].complete", is(true)))
-                .andExpect(jsonPath("$[0].user_id", is(1L)))
-                .andExpect(jsonPath("$[1].id", is(2L)))
-                .andExpect(jsonPath("$[1].content", is("Content2")))
-                .andExpect(jsonPath("$[1].name", is("Title2")))
-                .andExpect(jsonPath("$[1].complete", is(false)));
+                .andExpect(jsonPath("$[0].id", Matchers.is(1)))
+                .andExpect(jsonPath("$[0].content", Matchers.is("Content")))
+                .andExpect(jsonPath("$[0].name", Matchers.is("Title")))
+                .andExpect(jsonPath("$[0].complete", Matchers.is(true)))
+                //.andExpect(jsonPath("$[0].user", Matchers.is(1)))
+                .andExpect(jsonPath("$[1].id", Matchers.is(2)))
+                .andExpect(jsonPath("$[1].content", Matchers.is("Content2")))
+                .andExpect(jsonPath("$[1].name", Matchers.is("Title2")))
+                .andExpect(jsonPath("$[1].complete", Matchers.is(false)));
         Mockito.verify(noteService, Mockito.times(1)).getAllNotes();
         verifyNoMoreInteractions(noteService);
         Assert.assertEquals(noteService.getAllNotes(), sampleNotes);
@@ -137,7 +114,11 @@ public class NoteControllerTest {
 
     @Test
     public void testDeleteNote() throws Exception {
-
+        doNothing().when(noteService).deleteNote(sampleUser1.getId());
+        mockMvc.perform(delete("/notes/{id}", sampleUser1.getId()))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).deleteNote(sampleUser1.getId());
+        verifyNoMoreInteractions(noteService);
     }
 
     @Test
